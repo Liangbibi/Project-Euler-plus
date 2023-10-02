91991328faf0e2180d8aaf207a75c4dadddae469
Liang Jun Kai <v123tw0968050890liang@gmail.com>
2023-10-02 23:05:47 +0800

    Create 3.c

--- 3/3.c
+++ 3/3.c
@@ -2,7 +2,7 @@
 #include <stdbool.h>
 #include <math.h>
 
-bool isprime(unsigned long long n);
+bool is_prime(unsigned long long n);
 
 int main()
 {
@@ -17,7 +17,7 @@ int main()
         find = false;
         while (n!=1 && b<=sqrt(n)) {        // It is easy to know that we only interested in the first 2 max prime factors of one number
                                             // and the numbers we need to check are those smaller than or equal to sqrt(n).
-            while (n%b==0 && isprime(b)) {
+            while (n%b==0 && is_prime(b)) {
                 n /= b;
                 if (n==b) {
                     printf("%llu\n", n);    // If the first 2 max prime factors are equal, the max prime factor will be printf here.
@@ -32,7 +32,7 @@ int main()
     return 0;
 }
 
-bool isprime(unsigned long long n)      // To check whether the number that n divide is prime number. 
+bool is_prime(unsigned long long n)      // To check whether the number that n divide is prime number. 
 {
     unsigned long long i = 2;
     while (i<n) {
