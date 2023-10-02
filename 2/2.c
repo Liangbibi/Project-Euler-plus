3b1c100ffbb2d4c6da8fd8c82052e730a07eb9c7
Liang Jun Kai <v123tw0968050890liang@gmail.com>
2023-10-02 14:35:50 +0800

    Create 2.c

---
+++ 2/2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+    int i, j;
+    int a;
+    unsigned long long b, sum;
+    unsigned long long f[31] = {0};     // To storage even-value Fibonacci sequence.
+    f[0] = 2;
+    f[1] = 8;
+    for (i=2 ; i<31 ; i++)
+        f[i] = f[i-2] + f[i-1] * 4;     // It is easy to see that the formula of even-value Fibonacci sequence is f[i] = f[i-2] + f[i-1] * 4.
+    scanf("%d", &a);
+    for (i=0 ; i<a ; i++) {
+        scanf("%llu", &b);
+        sum = 0;
+        for (j=0 ; j<31 ; j++)
+            if (f[j]<=b)        // Calculate the sum.
+                sum += f[j];
+        printf("%llu\n", sum);
+    }
+    return 0;
+}
