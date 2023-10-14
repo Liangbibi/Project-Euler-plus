97c10027fb8b871eb8997ce77b9ccee81df69af9
Liang Jun Kai <v123tw0968050890liang@gmail.com>
2023-10-14 13:55:30 +0800

    Create 9.c

--- 009/9.c
+++ 009/9.c
@@ -16,7 +16,7 @@ int main()
         k = j - 1;
         l = b - j - k;
         while (j>b/3) {         // Because (k, l) = (a, b) and (b, a) are equal, thus, we let k always greater than or equal to l,
-                                // and that it is impossible for there are two numbers are equal, thus We let j > k > l.
+                                // and that it is impossible for there are two numbers are equal, thus we let j > k > l.
             while (j>k && j>l && k>l) {
                 if (j*j==k*k+l*l)           // To check whether (j, k, l) is a Pythagorean triplet.
                     if (j*k*l>max)
